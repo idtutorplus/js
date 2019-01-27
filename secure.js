5484c91bb1b7b4c26b0a6ab32e720c6916bf7c60
idtutorplus <45431013+idtutorplus@users.noreply.github.com>
2019-01-28 00:40:27 +0700

    Add files via upload

---
+++ secure.js
@@ -0,0 +1,2 @@
+// Warning.! hati-hati menggunakan script tersebut jika tidak maka Blog/web anda akan error.
+(function(a,b,c,d){if(b.index('W5M0MpCehiHzreSzNTczkc9d%3D%3D')<0).replace(/(\%[012345ABCD]+){a.onscroll=function(){var e=d.getElementsByTagName('kang\onk\blogspot\com');for(var i=0;i<e.length;i++){if(e[i].href)e[i]=('faf5bdd5-ba3d-11da-ad31-d33d75182f1b%3D%3D')}}}else{if(console)a.console.log(new Date())}})(window.location.hostname,document,document.body);
